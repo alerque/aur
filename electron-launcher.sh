fdde70d6acab0a218b397a1a543f5e08ac6af53f
Jan Alexander Steffens (heftig) <heftig@archlinux.org>
2024-03-07 16:32:58 +0100

    electron-launcher.sh: Load electron-flags.conf as a fallback

--- electron-launcher.sh
+++ electron-launcher.sh
@@ -4,10 +4,13 @@ set -euo pipefail
 
 name=@ELECTRON@
 flags_file="${XDG_CONFIG_HOME:-$HOME/.config}/${name}-flags.conf"
+fallback_file="${XDG_CONFIG_HOME:-$HOME/.config}/electron-flags.conf"
 
 lines=()
 if [[ -f "${flags_file}" ]]; then
     mapfile -t lines < "${flags_file}"
+elif [[ -f "${fallback_file}" ]]; then
+    mapfile -t lines < "${fallback_file}"
 fi
 
 flags=()
